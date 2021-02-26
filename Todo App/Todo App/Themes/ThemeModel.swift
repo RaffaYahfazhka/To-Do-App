9bc943c73a5a1ec5e13bcfc11b3dfe4c8cb1013d
RaffaYahfazhka <68719208+RaffaYahfazhka@users.noreply.github.com>
2021-02-26 11:26:15 +0700

    Add files via upload

---
+++ Todo App/Todo App/Themes/ThemeModel.swift
@@ -0,0 +1,15 @@
+//
+//  ThemeModel.swift
+//  Todo App
+//
+//  Created by Raffa Yahfazhka on 22/02/21.
+//
+
+import SwiftUI
+
+struct Theme: Identifiable {
+    let id: Int
+    let themeName: String
+    let themeColor: Color
+    
+}
